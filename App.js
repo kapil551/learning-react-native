ecca86e8a5ef019ea24fae0760ce8991c8067754
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-01-29 16:50:43 +0530

    refactored some code

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Start Learning React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,5 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+# React Native
+
+1. Official Docs:
+
+    - https://reactnative.dev/
+
+2. Setting up the development environment:
+
+    - https://reactnative.dev/docs/environment-setup
